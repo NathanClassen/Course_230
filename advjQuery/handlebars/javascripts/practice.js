b533a5b34285b580e6246cb78923cec010325b9b
Nathan Classen <hayesclassen@gmail.com>
2019-11-30 12:22:19 -0600

    third commit

---
+++ advjQuery/handlebars/javascripts/practice.js
@@ -0,0 +1,11 @@
+$(function() {
+  var post = {
+    title: 'Lorem ipsum dolor sit amet',
+    published: 'April 1, 2015',
+    body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.'
+  };
+
+  var postTemplate = Handlebars.compile($("#post").html());
+
+  $("body").append(postTemplate(post));
+});
